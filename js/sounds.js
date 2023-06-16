08d81cb083811548179a4f127a5ad70466cbae2e
Olena Babii <gracetomlinson26@gmail.com>
2023-06-17 00:57:59 +0300

    clean code

--- js/sounds.js
+++ js/sounds.js
@@ -1,15 +1,17 @@
-window.addEventListener("keydown", function (e) {
+function playSound(e) {
   const audio = this.document.querySelector(`audio[data-key=${e.code}]`);
   const key = this.document.querySelector(`.key[data-key=${e.code}]`);
   if (!audio) return;
   audio.play();
   audio.currentTime = 0;
   key.classList.add("playing");
-});
+}
+
+window.addEventListener("keydown", playSound);
 
 function removeTransition(e) {
-    if(e.propertyName !== 'transform') return;
-    this.classList.remove('playing');
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
 }
 
 const keys = document.querySelectorAll(".key");
